28f4e2c47d0cb69849cca120d214135d53e152a7
ryanwtl <104345724+ryanwtl@users.noreply.github.com>
2024-12-15 15:33:24 +0800

    Update multiple_chatbot.py

--- multiple_chatbot.py
+++ multiple_chatbot.py
@@ -39,7 +39,7 @@ def get_text_chunks(text):
     return text_splitter.split_text(text)
 
 def get_vector_store(text_chunks):
-    embeddings = hf_client.text_embeddings(model="sentence-transformers/all-MiniLM-L6-v2", inputs=texts)
+    embeddings = hf_client.text_embeddings(model="sentence-transformers/all-MiniLM-L6-v2")
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
@@ -66,7 +66,7 @@ def get_conversational_chain(model_name):
 
 def user_input(user_question, model_name):
     # embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    embeddings = hf_client.text_embeddings(model="sentence-transformers/all-MiniLM-L6-v2", inputs=texts)
+    embeddings = hf_client.text_embeddings(model="sentence-transformers/all-MiniLM-L6-v2")
     new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
     
